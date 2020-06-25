825ff181114099962c859b42447681ea38819f61
F4ilFish <47733499+F4ilFish@users.noreply.github.com>
2020-06-25 19:17:39 +0700

    Updates

---
+++ precache-manifest.9d08810828b58b9d222de5bea86e9267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7971af3e36ee7ce92eb11934ddc023",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "11d91afdaacc0812e4ff",
+    "url": "/recipe/static/css/main.a1fe5f02.chunk.css"
+  },
+  {
+    "revision": "abef057416bb10d65944",
+    "url": "/recipe/static/js/2.ad46b85a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe/static/js/2.ad46b85a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d91afdaacc0812e4ff",
+    "url": "/recipe/static/js/main.15e72802.chunk.js"
+  },
+  {
+    "revision": "ee67d5ed58402f46e7bd",
+    "url": "/recipe/static/js/runtime-main.bcf1e2d0.js"
+  }
+]);
